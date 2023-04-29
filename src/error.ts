375bcf9fb8a4e34c970a4e4493c9c3b5ea7e89ad
がっちゃん <yuki.dolphin.koga@gmail.com>
2023-04-29 15:58:49 +0000

    feat: エラーをSlackに通知する関数を追加

---
+++ src/error.ts
@@ -0,0 +1,34 @@
+function notifyErrorToSlack(
+  error,
+  config: {
+    botToken: string;
+    notifyChannelId: string;
+  }
+) {
+  const url = "https://slack.com/api/chat.postMessage";
+
+  const payload = {
+    channel: config.notifyChannelId,
+    text: `:warning: *エラーが発生しました* :warning:
+\`\`\`
+${error}
+\`\`\`
+`,
+  };
+
+  const params = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${config.botToken}`,
+    },
+    contentType: "application/json",
+    payload: JSON.stringify(payload),
+  } as unknown as GoogleAppsScript.URL_Fetch.URLFetchRequestOptions;
+
+  const fetchResult = UrlFetchApp.fetch(url, params);
+  const response = JSON.parse(fetchResult.getContentText());
+  if (!response.ok) {
+    throw new Error(`Slack API Error: ${response.error}`);
+  }
+  return response.ok;
+}
